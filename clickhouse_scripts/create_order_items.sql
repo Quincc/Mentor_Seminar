5f1326c35270f4f39e6f2f895514679b4cf4c972
Quincc <78716670+Quincc@users.noreply.github.com>
2025-04-21 15:10:00 +0500

    Update create_order_items.sql

--- clickhouse_scripts/create_order_items.sql
+++ clickhouse_scripts/create_order_items.sql
@@ -1,3 +1,4 @@
+-- Create Table order_items
 CREATE TABLE IF NOT EXISTS order_items
 (
     item_id UInt64,
@@ -7,3 +8,14 @@ CREATE TABLE IF NOT EXISTS order_items
     quantity UInt32
 ) ENGINE = MergeTree
 ORDER BY item_id;
+
+-- Insert into order_items
+INSERT INTO order_items
+SELECT *
+FROM s3(
+    'https://storage.yandexcloud.net/bucket-hse/raw/order_items/order_items.txt',
+    '<access_key>',
+    '<secret_key>',
+    'CSVWithNames'
+)
+SETTINGS format_csv_delimiter = ';';
